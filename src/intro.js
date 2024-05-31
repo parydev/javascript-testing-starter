b888d958527bbce40109971e916d8d9efb71f35f
parydev <parisafallahi71@gmail.com>
2024-05-31 16:39:15 +0330

    test

--- src/intro.js
+++ src/intro.js
@@ -4,11 +4,11 @@ export function max(a, b) {
   else if (b > a) return b;
   return a;
 }
-
+/////
 // Exercise
 export function fizzBuzz(n) {
-  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
-  if (n % 3 === 0) return 'Fizz';
-  if (n % 5 === 0) return 'Buzz';
+  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
+  if (n % 3 === 0) return "Fizz";
+  if (n % 5 === 0) return "Buzz";
   return n.toString();
 }
